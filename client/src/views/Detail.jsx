4928768a17bd4fa3686f04c41e03c86e91c2baaa
Paul De Ulloa <pauld143@gmail.com>
2023-09-13 11:30:07 -0700

    updates made to detail and update views

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -20,7 +20,11 @@ const Detail = (props) => {
         <p>Title: {product.title}</p>
         <p>Price: {product.price}</p>
         <p>Description: {product.description}</p>
+        <Link to={"/products/" + product._id + "/edit"}>
+    Edit
+</Link>
       </div>
+
   );
 };
 
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -25,3 +25,17 @@ module.exports.createProduct = (request, response) => {
     .then((product) => response.json(product))
     .catch((err) => response.status(400).json(err));
 };
+
+module.exports.updateProduct = (request, response) => {
+  Product.findOneAndUpdate({ _id: request.params.id }, request.body, {
+    new: true,
+  })
+    .then((updatedProduct) => response.json(updatedProduct))
+    .catch((err) => response.status(400).json(err));
+};
+
+module.exports.deleteProduct = (request, response) => {
+  Product.deleteOne({ _id: request.params.id })
+    .then((deleteConfirmation) => response.json(deleteConfirmation))
+    .catch((err) => response.status(400).json(err));
+};
