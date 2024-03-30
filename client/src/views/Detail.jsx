5b34e7b7be8882d99783f976b94899d4bf5324a6
Paul De Ulloa <pauld143@gmail.com>
2024-03-29 23:47:57 -0700

    main.jsx file updates

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -15,16 +15,12 @@ const Detail = (props) => {
   }, []);
 
   return (
-    
-      <div>
-        <p>Title: {product.title}</p>
-        <p>Price: {product.price}</p>
-        <p>Description: {product.description}</p>
-        <Link to={"/products/" + product._id + "/edit"}>
-    Edit
-</Link>
-      </div>
-
+    <div>
+      <p>Title: {product.title}</p>
+      <p>Price: {product.price}</p>
+      <p>Description: {product.description}</p>
+      <Link to={"/products/" + product._id + "/edit"}>Edit</Link>
+    </div>
   );
 };
 
--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,36 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import ProductForm from '../components/ProductForm';
-import ProductList from '../components/ProductList';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import ProductForm from "../components/ProductForm";
+import ProductList from "../components/ProductList";
+import axios from "axios";
 
 const Main = (props) => {
-    const [products, setProducts] = useState([]);
-    const [loaded, setLoaded] = useState(false);
-    
-    useEffect(()=>{
-        axios
-            .get('http://localhost:8000/api/products')
-            .then(res=>{
-                setProducts(res.data);
-                setLoaded(true);
-            })
-            .catch(err => console.error(err));
-    },[]);
+  const [products, setProducts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-    const removeFromDom = productId => {
-        setProducts(products.filter(product => product._id !== productId));
-    }
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/products")
+      .then((res) => {
+        setProducts(res.data);
+        setLoaded(true);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
+  const removeFromDom = (productId) => {
+    setProducts(products.filter((product) => product._id !== productId));
+  };
 
-    return (
-        <div>
-            <ProductForm />
-            <hr />
-            {loaded && <ProductList products={products} removeFromDom={removeFromDom}/>}
-        </div>
-        
-    )
-
-}
+  return (
+    <div id="detailsStyling">
+      <ProductForm />
+      <hr />
+      {loaded && (
+        <ProductList products={products} removeFromDom={removeFromDom} />
+      )}
+    </div>
+  );
+};
 export default Main;
