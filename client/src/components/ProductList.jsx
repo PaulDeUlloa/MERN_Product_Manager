31a7f552b37d1e3076efbcfeea5468e0c9c2ae30
Paul De Ulloa <pauld143@gmail.com>
2024-03-31 23:59:12 -0700

    allProducts list styling updated

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-    
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 const ProductList = (props) => {
-    const { removeFromDom } = props;
-    
-    const deleteProduct = (productId) => {
-        axios.delete('http://localhost:8000/api/products/' + productId)
-            .then(res => {
-                removeFromDom(productId)
-            })
-            .catch(err => console.error(err));
-    }
-    return (
-        <div>
-            <h2>All Products: </h2>
-            {props.products.map( (product, i) =>{
-                return (
-                    <p key={i}>
-                        <Link to={`/api/products/${product._id}`}>
-                        {product.title}
-                        </Link>
-                        {" "}
-                        <button onClick={(e)=>{deleteProduct(product._id)}}>
-                            Delete
-                        </button>
-                    </p>
-                );
-            })}
-        </div>
-    )
-}
-    
+  const { removeFromDom } = props;
+
+  const deleteProduct = (productId) => {
+    axios
+      .delete("http://localhost:8000/api/products/" + productId)
+      .then((res) => {
+        removeFromDom(productId);
+      })
+      .catch((err) => console.error(err));
+  };
+  return (
+    <div id="allProductsStyling">
+      <h2>All Products: </h2>
+      {props.products.map((product, i) => {
+        return (
+          <p key={i}>
+            <div id="listStyling">
+              <Link to={`/api/products/${product._id}`}>{product.title}</Link>{" "}
+              <button
+                onClick={(e) => {
+                  deleteProduct(product._id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
 export default ProductList;
