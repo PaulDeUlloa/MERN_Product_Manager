02ee5210f4e860bf7953de042ea1bebe459149e3
Paul De Ulloa <pauld143@gmail.com>
2024-04-17 23:48:09 -0700

    mongoose notes added.

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dbName = process.env.DB;
 const username = process.env.ATLAS_USERNAME;
 const pw = process.env.ATLAS_PASSWORD;
 const uri = `mongodb+srv://${username}:${pw}@productmanager.rc1o6jw.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
+//this is where we use mongoose to connect to our database.
 
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => console.log("Established a connection to the database"))
-    .catch(err => console.log("Something went wrong when connecting to the database", err));
+  })
+  .then(() => console.log("Established a connection to the database"))
+  .catch((err) =>
+    console.log("Something went wrong when connecting to the database", err)
+  );
